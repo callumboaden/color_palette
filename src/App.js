093c881c1cdbe93f089a05bf6b9214441b0fa842
callumboaden <boaden.callum@gmail.com>
2019-12-16 11:07:25 +0700

    added links to palettes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       <Switch>
               <Route 
                 exact path="/" 
-                render={() => <PaletteList palettes={seedColors} />} 
+                render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} 
               />
               <Route 
                 exact path="/palette/:id"
--- src/PaletteGenerator.js
+++ src/PaletteGenerator.js
@@ -10,12 +10,32 @@ class PaletteGenerator extends Component {
 
         this.state = { colors: [] };
     }
+    getColors() {
+        var proxy = "https://cors-anywhere.herokuapp.com/";
+        var url = `${proxy}http://colormind.io/api/`;
+        var data = {
+            model : "default",
+            input : [[44,43,44],[90,83,82],"N","N","N"]
+        }
+
+        var http = new XMLHttpRequest();
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                var palette = JSON.parse(http.responseText).result;
+                // console.log(palette)
+            }
+        }
+        console.log(data);
+        http.open("POST", url, true);
+        http.send(JSON.stringify(data));
+    }
     render() { 
         return ( 
             <div className="PaletteGenerator">
                 <h1>Palette Generator</h1>
 
-                <button onClick={this.generateColors}>Generate</button>
+                <button onClick={this.getColors}>Generate</button>
             </div> 
         );
     }
